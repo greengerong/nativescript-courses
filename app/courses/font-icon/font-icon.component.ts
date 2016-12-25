6c86375bc74a7e3d700e9e25ee811c4805b7d77c
polang <grzx2210@163.com>
2016-12-25 17:03:52 +0800

    [font-awesome] dynamic font icons

--- app/courses/font-icon/font-icon.component.ts
+++ app/courses/font-icon/font-icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FontIcon } from '../../core';
 
 @Component({
     selector: 'font-icon',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['courses/font-icon/font-icon.component.css']
 })
 export class FontIconComponent implements OnInit {
+    icons = [];
 
-    constructor() {
+    constructor(private fontIcon: FontIcon) {
 
+        this.icons = fontIcon.getAllIcon()
+            .map(icon => {
+                return {
+                    key: icon,
+                    value: fontIcon.getIconValue(icon)
+                };
+            });
     }
 
     ngOnInit() {
